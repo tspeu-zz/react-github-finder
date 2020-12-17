181b8ab98f7f61db78e16fc59fc5ac7a074be2e2
jm_b <jmbanchero@gmail.coom>
2020-12-17 19:13:00 +0000

    search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,23 +2,29 @@ import React, { Component } from 'react'
 
 export class Search extends Component {
 
+   // api search/users?q=userID
    state= {
       text: ''
    }
 
    onChange = (ev) =>this.setState({[ev.target.name]: ev.target.value});
    
-   onSubmit(ev){
+   // un prop se salida searchUsers
+   onSubmit = (ev) => {
       // para evitar burbujeo
       ev.preventDefault();
       console.log(this.state.text);
-
+      this.props.searchUsers(this.state.text);
+      //clear state
+      this.setState({ text: ''});
    }
+
 
    render() {
       return (
          <div>
-            <form onSubmit={this.onSubmit.bind(this)} className="form">
+            {/* bind(this) */}
+            <form onSubmit={this.onSubmit} className="form">
                <input type="text" name="text" placeholder="Search users..."
                value={this.state.text} onChange={this.onChange}/>
                {/* <input type="email" name="email" placeholder="ponga email..."
